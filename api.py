2ecad4c1957d55cf641a6ad1dcd17caed37fe2a3
karim-bouchez <bouchez.karim@gmail.com>
2023-07-06 21:26:17 +0200

    fix typo "run_discord_client"

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
-from bot import run_disord_client
+from bot import run_discord_client
 
 app = FastAPI()
 
@@ -29,6 +29,6 @@ class Payload(BaseModel):
 def start_bot(background_tasks: BackgroundTasks, payload: Payload):
     global is_bot_up
     if payload.force or not is_bot_up:
-        background_tasks.add_task(run_disord_client)
+        background_tasks.add_task(run_discord_client)
         is_bot_up = True
     return {"is_bot_up": is_bot_up}
